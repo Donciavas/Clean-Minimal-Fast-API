aab6f72c227a08895293100e073ebb46f31d0484
Donatas Vitkus <vitk.donatas@gmail.com>
2023-03-10 18:58:29 +0200

    Add locally build program

---
+++ Customers.Api/Program.cs
@@ -0,0 +1,39 @@
+using Customers.Api.Contracts.Responses;
+using Customers.Api.Database;
+using Customers.Api.Repositories;
+using Customers.Api.Services;
+using FastEndpoints;
+using FastEndpoints.Swagger;
+
+var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+
+builder.Services.AddFastEndpoints();
+builder.Services.AddSwaggerDoc();
+
+builder.Services.AddSingleton<IDbConnectionFactory>(_ =>
+    new SqliteConnectionFactory(config.GetValue<string>("Database:ConnectionString")));
+builder.Services.AddSingleton<DatabaseInitializer>();
+builder.Services.AddSingleton<ICustomerRepository, CustomerRepository>();
+builder.Services.AddSingleton<ICustomerService, CustomerService>();
+
+var app = builder.Build();
+
+app.UseFastEndpoints(x =>
+{
+    x.Errors.ResponseBuilder = (failures, ctx, statusCode) =>
+    {
+        return new ValidationFailureResponse
+        {
+            Errors = failures.Select(y => y.ErrorMessage).ToList()
+        };
+    };
+});
+
+app.UseOpenApi();
+app.UseSwaggerUi3(s => s.ConfigureDefaults());
+
+var databaseInitializer = app.Services.GetRequiredService<DatabaseInitializer>();
+await databaseInitializer.InitializeAsync();
+
+app.Run();
